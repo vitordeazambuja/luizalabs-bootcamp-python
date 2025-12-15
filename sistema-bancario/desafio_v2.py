e8739794243b345f425b7f8cedb48e29fec6ded0
Vitor <vitordeazambuja@edu.uniube.br>
2025-12-15 15:08:19 -0300

    feat: lógica das classes foi ajustada, implementando as funcionalidades desejadas

--- sistema-bancario/desafio_v2.py
+++ sistema-bancario/desafio_v2.py
@@ -18,20 +18,32 @@ class Deposito(Transacao):
     def __init__(self, valor):
         self._valor = valor
     
-    def registrar(self, conta):
-        conta.depositar(self._valor)
-        pass
+    @property
+    def valor(self):
+        return self.valor
+    
+    def registrar(self,conta):
+        if conta.depositar(self._valor):
+            conta._historico.adicionar_transacao(self)
 
 class Saque(Transacao):
     def __init__(self, valor):
         self._valor = valor
 
-    def registrar(self, conta):
-        pass
+    @property
+    def valor(self):
+        return self.valor
+    
+    def registrar(self,conta):
+        if conta.sacar(self._valor):
+            conta._historico.adicionar_transacao(self)
 
 class Historico:
+    def __init__(self):
+        self._log = []
+    
     def adicionar_transacao(self, transacao):
-        pass
+        self._log.append(transacao)
 
 class Conta:
     def __init__(self, numero, agencia, cliente, historico):
@@ -50,25 +62,33 @@ class Conta:
         return Conta(numero,AGENCIA,cliente,Historico())
 
     def sacar(self, valor):
-        pass
+        if valor <= 0:
+            return False
+    
+        if valor > self._saldo:
+            return False
+        
+        self._saldo -= valor
+        return True
 
-    def depositar(self, valor):
-        pass
+    def depositar(self, valor):     
+        self._saldo += valor
+        return True
 
 class ContaCorrente(Conta):
     def __init__(self,numero, agencia, cliente, historico, limite, limite_saques):
-        super().__init__
+        super().__init__(numero, agencia, cliente, historico)
         self._limite = limite
         self._limite_saques = limite_saques
 
 class Cliente(PessoaFisica):
-    def __init__(self, endereco, contas):
-        super().__init__
+    def __init__(self,cpf, nome, data_nascimento, endereco):
+        super().__init__(cpf, nome, data_nascimento)
         self._endereco = endereco
-        self._contas = contas
+        self._contas = []
     
     def realizar_transacao(self, conta, transacao):
-        pass
+        transacao.registrar(conta)
 
     def adicionar_conta(self, conta):
-        pass
+        self._contas.append(conta)
