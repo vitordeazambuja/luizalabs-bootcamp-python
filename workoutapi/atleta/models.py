86f12c87b6005320f4342f5a6acb4ff01a14b127
Vitor <vitordeazambuja@edu.uniube.br>
2025-12-17 10:53:23 -0300

    feat: projeto novo de workoutapi - criacao de configuracoes do projeto e schemas e model da entidade atleta

---
+++ workoutapi/atleta/models.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from sqlalchemy import Integer, String, Float, DateTime
+from sqlalchemy.orm import Mapped, mapped_column
+from workoutapi.contrib.models import BaseModel
+
+class AtletaModel(BaseModel):
+    __tablename__ = 'atletas'
+
+    pk_id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    nome: Mapped[str] = mapped_column(String(50), nullable=False)
+    cpf: Mapped[str] = mapped_column(String(11), nullable=False)
+    idade: Mapped[int] = mapped_column(Integer, nullable=False)
+    peso: Mapped[float] = mapped_column(Float, nullable=False)
+    altura: Mapped[float] = mapped_column(Float, nullable=False)
+    sexo: Mapped[str] = mapped_column(DateTime, nullable=False)
---
+++ workoutapi/atleta/schemas.py
@@ -0,0 +1,10 @@
+from typing import Annotated
+from pydantic import BaseModel, Field, PositiveFloat
+
+class Atleta(BaseModel):
+    nome: Annotated[str, Field(description='Nome do atleta', examples=['Joao'], max_length=50)]
+    cpf: Annotated[str, Field(description='CPF do atleta', examples=['12345678900'], max_length=11)]
+    idade: Annotated[int, Field(description='Idade do atleta', examples=[25])]
+    peso: Annotated[PositiveFloat, Field(description='Peso do atleta', examples=[75.5])]
+    altura: Annotated[PositiveFloat, Field(description='Altura do atleta', examples=[1.70])]
+    sexo: Annotated[str, Field(description='Sexo do atleta', examples=['M'], max_length=1)]
