4037ec88e146e6de6961a5645db64bcde32619bf
Vitor <vitordeazambuja@gmail.com>
2025-12-28 18:27:42 -0300

    feat: Implementado servico de transacoes no controller de contas

--- api-bancaria/accounts/controller.py
+++ api-bancaria/accounts/controller.py
@@ -5,8 +5,11 @@ from accounts.schema import AccountOut, DepositIn, WithdrawIn
 from accounts.service import AccountService
 from core.security import get_current_user
 
+from transactions.service import TransactionService
+
 router = APIRouter(prefix="/accounts", tags=["Accounts"])
 service = AccountService()
+transaction_service = TransactionService()
 
 
 @router.post("/me/deposit", response_model=AccountOut)
@@ -16,6 +19,12 @@ async def deposit(
 ):
     try:
         balance = await service.deposit(user_id=user["id"], amount=data.amount)
+        account = await service.get_by_user(user["id"])
+
+        if account is None:
+            raise HTTPException(status_code=404, detail="Conta não encontrada")
+        
+        await transaction_service.create(account["id"], "deposit", data.amount)
         return {"id": user["id"], "balance": balance}
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
@@ -28,6 +37,12 @@ async def withdraw(
 ):
     try:
         balance = await service.withdraw(user_id=user["id"], amount=data.amount)
+        account = await service.get_by_user(user["id"])
+
+        if account is None:
+            raise HTTPException(status_code=404, detail="Conta não encontrada")
+
+        await transaction_service.create(account["id"], "withdraw", data.amount)
         return {"id": user["id"], "balance": balance}
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
